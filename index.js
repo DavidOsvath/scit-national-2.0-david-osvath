154b491d23bf6756a3ee098a3d00dc207b4cd370
David Osvath <osvathdavid@yahoo.com>
2021-03-23 20:55:42 +0200

    moving added

---
+++ index.js
@@ -0,0 +1,53 @@
+console.log("JavaScript OOP Game");
+
+//object litteral
+// this comes from Object class
+
+const obj = {};
+
+const a = new Object();
+console.log(a);
+
+//this comes from Array class
+const b = [];
+
+class Car {
+    constructor(color) {
+        this.color = color;
+    }
+
+    startCar() {
+        console.log("Start Car");
+    }
+}
+
+class CarWithElectricWindows extends Car {
+       openElectricWindow(windowNr){
+        console.log("Window nr",windowNr,"opened");
+    }
+}
+
+const car1 = new Car("white");
+console.log(car1);
+
+const car2 = new CarWithElectricWindows("black");
+console.log(car2);
+car2.startCar();
+car2.openElectricWindow(3);
+
+
+class MyArray extends Array {
+    printLengthWithJoy() {
+        console.log(this.length + " with joy");
+    }
+}
+
+
+const myArray1 = new MyArray();
+myArray1.push("abc");
+console.log(myArray1);
+myArray1.printLengthWithJoy();
+
+
+
+
