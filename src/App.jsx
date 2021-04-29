21953f47e0c20e300be3743400dab5bdfeaa4e8f
David Osvath <osvathdavid@yahoo.com>
2021-04-29 20:05:17 +0300

    removed node modules

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Header } from "./components/Header/Header";
+
+import "./App.css";
+import { Home } from "./pages/Home/Home";
+import { About } from "./pages/About/About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+ function App() { 
+    return (
+      <Router>
+        <div className="App" id="app">
+          <Header />  
+          <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>      
+        </div>
+      </Router>
+    );
+}
+
+export default App;
