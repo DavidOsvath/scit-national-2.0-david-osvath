42718ad4b1249a63139162782487e0df9c296043
David Osvath <osvathdavid@yahoo.com>
2021-05-02 04:53:47 +0300

    clean start

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import { AppLogo } from "../AppLogo/AppLogo";
-import { Link } from "react-router-dom";
 import "./Header.css";
 
 export function Header() {
@@ -7,10 +6,6 @@ export function Header() {
     <div className="app-header">
       <AppLogo />
       <p className="app-header__title">To Do App</p>
-      <div className="navigation">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </div>
     </div>
   );
 }
